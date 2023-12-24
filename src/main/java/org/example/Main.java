365e0609ea6571ab7a5d9183ff006c255550cd0d
koalaa13 <maksimov.n@sel-be.ru>
2023-12-24 11:36:59 +0300

    download map endpoint

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.api.ApiController;
 import org.example.game.Game;
 import org.example.model.IslandMap;
 import org.example.model.Scan;
@@ -14,37 +15,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    private static void downloadMap() {
+        ApiController apiController = new ApiController();
+        apiController.downloadMapFile(apiController.getMapUrl());
+    }
+
     public static void main(String[] args) throws InterruptedException, JsonProcessingException {
-        MapParser mapParser = new MapParser();
-        IslandMap islandMap = mapParser.parseIslandMap();
-        Ship myShip = new Ship();
-        myShip.setId(1L);
-        myShip.setX(20);
-        myShip.setY(20);
-        myShip.setSpeed(10);
-        myShip.setMaxChangeSpeed(20);
-        myShip.setCannonRadius(100);
-        myShip.setSize(5);
-        myShip.setDirection(Ship.Direction.north);
-        List<Ship> myShips = new ArrayList<>();
-        myShips.add(myShip);
+        downloadMap();
 
-        Ship enemyShip = new Ship();
-        enemyShip.setId(2L);
-        enemyShip.setX(30);
-        enemyShip.setY(30);
-        enemyShip.setSize(5);
-        enemyShip.setDirection(Ship.Direction.east);
-        List<Ship> enemyShips = new ArrayList<>();
-        enemyShips.add(enemyShip);
-
-        Scan scan = new Scan();
-        scan.setMyShips(myShips);
-        scan.setEnemyShips(enemyShips);
-
-        ShipCommands shipCommands = new Game(islandMap).makeShipCommands(scan);
-        System.out.println(new ObjectMapper().writeValueAsString(shipCommands));
-
+//        MapParser mapParser = new MapParser();
+//        IslandMap islandMap = mapParser.parseIslandMap();
+//        Ship myShip = new Ship();
+//        myShip.setId(1L);
+//        myShip.setX(20);
+//        myShip.setY(20);
+//        myShip.setSpeed(10);
+//        myShip.setMaxChangeSpeed(20);
+//        myShip.setCannonRadius(100);
+//        myShip.setSize(5);
+//        myShip.setDirection(Ship.Direction.north);
+//        List<Ship> myShips = new ArrayList<>();
+//        myShips.add(myShip);
+//
+//        Ship enemyShip = new Ship();
+//        enemyShip.setId(2L);
+//        enemyShip.setX(30);
+//        enemyShip.setY(30);
+//        enemyShip.setSize(5);
+//        enemyShip.setDirection(Ship.Direction.east);
+//        List<Ship> enemyShips = new ArrayList<>();
+//        enemyShips.add(enemyShip);
+//
+//        Scan scan = new Scan();
+//        scan.setMyShips(myShips);
+//        scan.setEnemyShips(enemyShips);
 //        Visualizer visualizer = new Visualizer(islandMap, enemyShips, myShips);
 //        visualizer.visualize(0, 0, 60);
     }
--- src/main/java/org/example/api/ApiController.java
+++ src/main/java/org/example/api/ApiController.java
@@ -1,6 +1,7 @@
 package org.example.api;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
@@ -12,7 +13,17 @@ import org.example.model.response.ScanResponse;
 import org.example.model.command.ShipCommands;
 import org.example.model.response.DefaultApiResponse;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.Buffer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ApiController {
     private static final String API_URL = "https://datsblack.datsteam.dev/api";
@@ -80,5 +91,36 @@ public class ApiController {
             System.err.println(e);
         }
         return defaultApiResponse;
+    }
+
+    public String getMapUrl() {
+        String link = null;
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            HttpGet request = new HttpGet(API_URL + "/map");
+            request.setHeader(API_AUTH_HEADER, API_KEY);
+
+            link = client.execute(request, response -> objectMapper.readTree(response.getEntity().getContent())).get("mapUrl").asText();
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        return link;
+    }
+
+    public void downloadMapFile(String link) {
+        try {
+            URL url = new URL(link);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            BufferedWriter writer = Files.newBufferedWriter(Paths.get("src/main/resources/island_map.json"), StandardCharsets.UTF_8);
+
+            String inputLine;
+            while ((inputLine = reader.readLine()) != null) {
+                writer.write(inputLine);
+                writer.newLine();
+            }
+            reader.close();
+            writer.close();
+        } catch (IOException e) {
+            System.err.println(e);
+        }
     }
 }
