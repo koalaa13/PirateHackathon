de11546a402057162329a094055304729f44a628
alex-shishkin <alex-shishkin@yandex-team.ru>
2023-12-24 12:47:00 +0300

    Add zoom

--- src/main/java/org/example/visual/GraphVisualizer.java
+++ src/main/java/org/example/visual/GraphVisualizer.java
@@ -13,6 +13,7 @@ import java.util.Iterator;
 import java.util.List;
 
 public class GraphVisualizer extends JFrame {
+    private double zoom = 0.45;
     private IslandMap islandMap;
 
     private IslandMap.Tiles myShipsTiles = new IslandMap.Tiles();
@@ -34,15 +35,15 @@ public class GraphVisualizer extends JFrame {
 
         this.islandMap = islandMap;
 
-        int W = (int) islandMap.getWidth();
-        int H = (int) islandMap.getHeight();
+        int W = (int) (islandMap.getWidth() + 2);
+        int H = (int) (islandMap.getHeight() + 2);
 
-        setSize(W, H);
+        setSize((int) Math.ceil(W * zoom), (int) Math.ceil(H * zoom));
 
         addMouseListener(new MouseListener(){
             public void mouseClicked(MouseEvent e){
-                selectedX = e.getX();
-                selectedY = e.getYOnScreen();
+                selectedX = (int) (e.getX() / zoom);
+                selectedY = (int) ((e.getY() - 25) / zoom);
                 System.out.printf("Move to %d %d\n", selectedX, selectedY);
                 updateGraph();
             }
@@ -59,17 +60,20 @@ public class GraphVisualizer extends JFrame {
             public void paint(Graphics g)
             {
                 // set color to red
-                g.setColor(new Color(0, 200, 0));
-                g.drawRect((int) selectedX, (int) selectedY, 3, 3);
+                g.setColor(new Color(0, 180, 0));
+                g.drawRect((int) (selectedX * zoom), (int) (selectedY * zoom), 2, 2);
 
-                g.setColor(new Color(0, 0, 200));
+                g.setColor(new Color(0, 0, 180));
                 paintTiles(myShipsTiles, g);
 
-                g.setColor(new Color(200, 0, 0));
+                g.setColor(new Color(180, 0, 0));
                 paintTiles(enemyShipsTiles, g);
 
-                g.setColor(new Color(200, 200, 0));
+                g.setColor(new Color(180, 180, 0));
                 paintTiles(islandMap.getRawTiles(), g);
+
+                g.setColor(new Color(230, 230, 230));
+                g.drawRect((int) (1000 * zoom), (int) (1000 * zoom), 2, 2);
             }
         };
 
@@ -81,7 +85,7 @@ public class GraphVisualizer extends JFrame {
         Iterator<Tile> it = tiles.iterateTiles();
         while (it.hasNext()) {
             Tile tile = it.next();
-            g.drawRect((int) tile.getX(), (int) tile.getY(), 1, 1);
+            g.drawRect((int) (tile.getX() * zoom), (int) (tile.getY() * zoom), 1, 1);
         }
     }
 
